a3368257b3f598b057350e7aa1500fb7ca6a4880
Erdal <erdalozkaya71@hotmail.com>
2023-10-01 15:12:38 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// Middleware to parse JSON request bodies
+app.use(bodyParser.json());
+
+// GET request: /hello
+app.get('/hello', (req, res) => {
+  res.send('Hello Express JS');
+});
+
+//http://localhost:3000/user?firstname=Erdal&lastname=Ozkaya
+// GET request: /user with query parameters
+app.get('/user', (req, res) => {
+  const { firstname, lastname } = req.query;
+  const userData = {
+    firstname,
+    lastname,
+  };
+  res.json(userData);
+});
+
+//http://localhost:8089/user/Erdal/Ozkaya
+// POST request: /user with path parameters
+app.post('/user/:firstname/:lastname', (req, res) => {
+  const { firstname, lastname } = req.params;
+  const userData = {
+    firstname,
+    lastname,
+  };
+  res.json(userData);
+});
+
+// Start the server
+const port = 8089;
+app.listen(port, () => {
+  console.log(`Server is listening on port ${port}`);
+});
